601823e8c65b3fe1c59f43b6112922797945bbf2
H-Akaogi <hazumi.akaogi@wemex.com>
2026-05-21 10:55:56 +0900

    Presentations-ViewModels作成

--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67992f89dd03676bae9035ab743604ee7aca2d27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28f100cefe24a7350559a75bd6645d2ab7c72b73")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
