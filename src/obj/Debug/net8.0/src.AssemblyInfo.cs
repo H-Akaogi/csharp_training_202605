3009303ed3339c25c61160885f3a8ca00a55c9cd
H-Akaogi <hazumi.akaogi@wemex.com>
2026-05-21 13:32:24 +0900

    表示調整

--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab39767eee87cd14bebe1bf0eab59ca7b8002a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1704283f5176a6968dde399a629a12a92d68621")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
